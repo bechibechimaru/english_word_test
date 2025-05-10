e7f45e5e53d8f54b017862d2c6ccdd3aa7050866
becchi <mebiusu1968@gmail.com>
2025-05-11 01:36:19 +0900

    frontのアクセス権限を更新

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      "ec2-54-178-56-216.ap-northeast-1.compute.amazonaws.com"
+    ]
+  }
 })
